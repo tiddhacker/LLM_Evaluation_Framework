c317c8242b8739875510f3540400a43684742364
Sounak Ghosh <sounak.ghosh2@gds.ey.com>
2025-04-03 17:14:22 +0530

    llm eval working

--- pages/homePage.py
+++ pages/homePage.py
@@ -1,5 +1,6 @@
 from .basePage import BasePage
 from llmEvaluatorUtil import *
+from llmtesteval import evaluate_dataset, generateEvaluationReport
 
 class HomePage(BasePage):
     def __init__(self, page):
@@ -15,5 +16,15 @@ class HomePage(BasePage):
         self.page.close()  # Closing the browser page
 
     async def testLLM(self):
-        sampleFinal = createSampleDataSet("This is Java", "This is Java")
-        score = await fetch_score_with_retry(sampleFinal)
+        # sampleFinal = createSampleDataSet("This is Java", "This is Java")
+        # score = await fetch_score_with_retry(sampleFinal)
+        data_samples = {
+            'question': ['When was the first super bowl?'],
+            'answer': ['The first Super Bowl was not held'],
+            'contexts': [
+              ['java is the best.']
+            ],
+            'reference': ['Super Bowl I took place on January 15, 1967.']
+        }
+        result_set=evaluate_dataset(data_samples)
+        generateEvaluationReport("testReport",result_set)
