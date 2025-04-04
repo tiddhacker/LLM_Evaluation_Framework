142b3c14521c1c43cc75acc7d83a85cd9e0b96b2
Sounak Ghosh <sounak.ghosh2@gds.ey.com>
2025-04-04 18:41:14 +0530

    code refactoring

--- features/sample.feature
+++ features/sample.feature
@@ -4,4 +4,4 @@ Feature: Sample Feature with Playwright and POM
   Scenario: Verify homepage title test
     Given I open the homepage
     Then I should see the homepage title
-    Then I verify the factual correctness
+    Then I verify the LLM response
--- features/steps/sampleSteps.py
+++ features/steps/sampleSteps.py
@@ -15,11 +15,23 @@ async def verify_homepage_title(context):
     print(context.page.get_homepage_title())
 
 
-# Use async_run_until_complete to properly handle async steps
-@then('I verify the factual correctness')
+# # Use async_run_until_complete to properly handle async steps
+# @then('I verify the factual correctness')
+# @async_run_until_complete
+# async def step_impl(context):
+#      # Load and concatenate context PDFs
+#     question= "Can lambdas return a value ?"
+#     answer="Yes, lambdas can return a value. The type of the return value will be inferred by the compiler. The return statement is not required if the lambda body is just a one-liner"
+#     reference="Lambdas may return a value. The type of the return value will be inferred by compiler. The return statement is not required if the lambda body is just a one-liner. "
+#     context1 = read_pdf("context_files/Java_8.pdf")
+#     # context2 = read_pdf("context_files/javanotes8.pdf")
+#     full_context = context1
+#     await context.page.testLLM(full_context,question,answer,reference)
+
+@then('I verify the LLM response')
 @async_run_until_complete
-async def step_impl(context):
-     # Load and concatenate context PDFs
+async def step_then(context):
+   # Load and concatenate context PDFs
     question= "Can lambdas return a value ?"
     answer="Yes, lambdas can return a value. The type of the return value will be inferred by the compiler. The return statement is not required if the lambda body is just a one-liner"
     reference="Lambdas may return a value. The type of the return value will be inferred by compiler. The return statement is not required if the lambda body is just a one-liner. "
