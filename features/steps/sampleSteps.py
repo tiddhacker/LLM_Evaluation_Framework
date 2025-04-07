7bb9198bb82102676a730fe815d970b624c08953
Sounak Ghosh <sounak.ghosh2@gds.ey.com>
2025-04-07 13:53:55 +0530

    adding scenario outline

--- features/steps/sampleSteps.py
+++ features/steps/sampleSteps.py
@@ -14,14 +14,14 @@ async def open_homepage(context):
 async def verify_homepage_title(context):
     print(context.page.get_homepage_title())
 
-@then('I verify the LLM response')
+@then('I evaulate response for "{question}" "{answer}" "{reference}" "{context_reference}"')
 @async_run_until_complete
-async def step_then(context):
-   # Load and concatenate context PDFs
-    question= "Can lambdas return a value ?"
-    answer="Yes, lambdas can return a value. The type of the return value will be inferred by the compiler. The return statement is not required if the lambda body is just a one-liner"
-    reference="Lambdas may return a value. The type of the return value will be inferred by compiler. The return statement is not required if the lambda body is just a one-liner. "
-    context1 = read_pdf("context_files/Java_8.pdf")
+async def step_impl(context, question,answer,reference,context_reference):
+    # Load and concatenate context PDFs
+    ques= question
+    ans=answer
+    ref=reference
+    context1 = read_pdf(context_reference)
     # context2 = read_pdf("context_files/javanotes8.pdf")
     full_context = context1
-    await context.page.testLLM(full_context,question,answer,reference)
+    await context.page.testLLM(full_context,ques,ans,ref)
